feee3fdffadccce6d323b8c03814b4814d24f080
wu kai <wukai2048@gmail.com>
2019-03-17 22:41:33 +0800

    添加 router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import ScrollTop from './components/scrollTop';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
-import logo from './logo.svg';
 import DemoScrollTop from './containers/demo-scroll-top';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <DemoScrollTop />
+        <Router>
+          <Route path="/scroll-top" component={DemoScrollTop} />
+        </Router>
       </div>
     );
   }
